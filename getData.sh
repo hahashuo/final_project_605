2bfb10ad23e6af828cc56cb47c2d88e6de869608
hahashuo <77724060+hahashuo@users.noreply.github.com>
2021-12-03 14:27:12 -0600

    Revised getData.sh

--- getData.sh
+++ getData.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-wget -qO- https://archive.org/download/stackexchange |
-sed -e 's/<[^>]*>//g' |
-grep 'com.7z' |
-awk '$0 !~ /meta/' |
-tr -d [:blank:] |
-sed 's/.\{14\}$//' > filename
-
-for item in $(cat filename); do wget https://archive.org/download/stackexchange/$item/Posts.xml -O $item.Posts.xml; done
+for topic in $(cat temp.txt)
+do
+    echo $topic
+    wget https://archive.org/download/stackexchange/$topic/Posts.xml -O $topic.Posts.xml
+    cat $topic.Posts.xml | grep -Pio 'Id="1".*Body="\K[^"]*' | sed 's/;/\n/g' | awk '$0 ~ /&lt/' | sed 's/&lt//g' | tr " " "\n" | sed '/\S/!d' > $topic.csv
+    split -d -l 105000 $topic.csv "$topic.csv."
+done
