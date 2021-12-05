4cd4538860dbc9f9dbd9cdffceeb8400278ccd44
hahashuo <77724060+hahashuo@users.noreply.github.com>
2021-12-05 15:17:45 -0600

    Revise getData

--- getData.sh
+++ getData.sh
@@ -4,6 +4,7 @@ for topic in $(cat temp.txt)
 do
     echo $topic
     wget https://archive.org/download/stackexchange/$topic/Posts.xml -O $topic.Posts.xml
-    cat $topic.Posts.xml | grep -Pio 'Id="1".*Body="\K[^"]*' | sed 's/;/\n/g' | awk '$0 ~ /&lt/' | sed 's/&lt//g' | tr " " "\n" | sed '/\S/!d' > $topic.csv
-    split -d -l 105000 $topic.csv "$topic.csv."
+    cat $topic.Posts.xml | grep -Pio '.*Body="\K[^"]*' | sed 's/;/\n/g' | awk '$0 ~ /&lt/' | sed 's/&lt//g' | tr " " "\n" | sed '/\S/!d' > $topic.csv
+    split -d -l 210000 $topic.csv "$topic.csv."
 done
+rm *.xml
