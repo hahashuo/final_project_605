38cf52b73d729d44d3cf05e83b370a7509fe60e9
Jack Bressett <38769118+BressettJ21@users.noreply.github.com>
2021-12-05 21:43:54 -0600

    Files now save to particular directories

--- getData.sh
+++ getData.sh
@@ -1,10 +1,13 @@
-#!/bin/bash
+#!/bin/bash                                                                                                                                                                                                
 
 for topic in $(cat temp.txt)
 do
+    mkdir $topic
     echo $topic
     wget https://archive.org/download/stackexchange/$topic/Posts.xml -O $topic.Posts.xml
-    cat $topic.Posts.xml | grep -Pio '.*Body="\K[^"]*' | sed 's/;/\n/g' | awk '$0 ~ /&lt/' | sed 's/&lt//g' | tr " " "\n" | sed '/\S/!d' > $topic.csv
-    split -d -l 210000 $topic.csv "$topic.csv."
+    cat $topic.Posts.xml | grep -Pio '.*Body="\K[^"]*' | sed 's/;/\n/g' | awk '$0 ~ /&lt/' | sed 's/&lt//g' | tr " " "\n" | sed '/\S/!d' > $topic/$topic.csv
+    split -d -l 210000 $topic/$topic.csv "$topic/$topic.csv."
+    rm $topic/$topic.csv
 done
 rm *.xml
+
