ca633ceb341aa41e2b18dd6286b8276f336438f4
Jack Bressett <38769118+BressettJ21@users.noreply.github.com>
2021-12-05 21:57:42 -0600

    Groups TF CSVs into directories by topic
    This prepares the data for the second batch of jobs to merge the TF files per topic.

---
+++ groupFiles.sh
@@ -0,0 +1,9 @@
+#!bin/bash                                                                                                                                                                                                 
+
+for entry in temp.txt
+do
+    for name in $(find -iname "$entry.csv*")
+    do
+        mv $name home/bressett/$entry     #Change to your NetID
+    done
+done
