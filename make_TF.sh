c1dd5d7e937cf1b460b10c9d02d3107bec12e50c
Jack Bressett <38769118+BressettJ21@users.noreply.github.com>
2021-12-05 21:50:58 -0600

    Sh to be executed by sub file
    Calls the cleanData.R file

---
+++ make_TF.sh
@@ -0,0 +1,15 @@
+#!/bin/bash                                                                                                                                                                                                
+
+# untar your R installation                                                                                                                                                                                
+tar -xzf R402.tar.gz
+tar -xzf textstem_0.1.4.tar.gz
+
+# make sure the script will use your R installation,                                                                                                                                                       
+# and the working directory as its home location                                                                                                                                                           
+export PATH=$PWD/R/bin:$PATH
+export RHOME=$PWD/R
+export R_LIBS=$PWD/packages
+
+# run your script                                                                                                                                                                                          
+Rscript cleanData.R $1
+
