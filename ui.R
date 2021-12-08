a4af6b4498ad296daf908c68eab0f172ad1cbc5e
Jack Bressett <38769118+BressettJ21@users.noreply.github.com>
2021-12-08 14:54:19 -0600

    ui file for shiny app

---
+++ ui.R
@@ -0,0 +1,66 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinyWidgets)
+curvePlotsData <- read.csv("linePLot-formatData.csv")
+curvePlotsData <- curvePlotsData[,-1] #remove needless index variable
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Natural Language in the Stack Exchange Network: An Analysis on Frequencies of the Top 100 Most Used Words per Topic"),
+    navbarPage("Welcome",
+               tabPanel("Explore Zipf's Law",
+                    sidebarLayout( # Sidebar 
+                        sidebarPanel(                
+                            multiInput(
+                            inputId = "topics",
+                            label = "Select which topics you would like to see", 
+                            choices = colnames(curvePlotsData),
+                            choiceNames = colnames(curvePlotsData),
+                            choiceValues = colnames(curvePlotsData),
+                        )),
+                            
+                        # Show a plot of the generated distribution
+                        mainPanel(
+                            plotOutput("distPlot"),
+                        )  
+                    ),
+                ), #closes first tab
+                tabPanel("See the Important Words",
+                         # Sidebar with a slider input for number of bins
+                         sidebarLayout(
+                             sidebarPanel(
+                                 numericInput(
+                                     inputId = "numRows",
+                                     label = "Select how many rows you would like [1-100]",
+                                     value = 15,
+                                     min=1,
+                                     max = 100,
+                                     step = 1
+                                     ),
+                                 multiInput(
+                                     inputId = "topicsFreq",
+                                     label = "Select which topics you would like to see", 
+                                     choices = colnames(curvePlotsData),
+                                     choiceNames = colnames(curvePlotsData),
+                                     choiceValues = colnames(curvePlotsData),
+                                 )),
+                             
+                             # Show a plot of the generated distribution
+                             mainPanel(
+                                 tableOutput("mostFreq"),
+                                 
+                             )  
+                         ),
+                ) #close second tab
+    ) #Close Navbar
+)) #Close Shiny UI
