91a85452b89db953df6a51302b0fb3add7795569
Jack Bressett <38769118+BressettJ21@users.noreply.github.com>
2021-12-08 22:06:09 -0600

    Adds a homepage with text

--- ui.R
+++ ui.R
@@ -19,6 +19,20 @@ shinyUI(fluidPage(
     # Application title
     titlePanel("Natural Language in the Stack Exchange Network: An Analysis on Frequencies of the Top 100 Most Used Words per Topic"),
     navbarPage("Welcome",
+               tabPanel(icon("home"),
+                        
+                            p("p creates a paragraph of text."),
+                            p("A new p() command starts a new paragraph. Supply a style attribute to change the format of the entire paragraph.", style = "font-family: 'times'; font-si16pt"),
+                            strong("strong() makes bold text."),
+                            em("em() creates italicized (i.e, emphasized) text."),
+                            br(),
+                            code("code displays your text similar to computer code"),
+                            div("div creates segments of text with a similar style. This division of text is all blue because I passed the argument 'style = color:blue' to div", style = "color:blue"),
+                            br(),
+                            p("span does the same thing as div, but it works with",
+                              span("groups of words", style = "color:blue"),
+                              "that appear inside a paragraph.")
+               ),
                tabPanel("Explore Zipf's Law",
                     sidebarLayout( # Sidebar 
                         sidebarPanel(                
